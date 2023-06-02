c2b2de0e0233e636e28b5ffc165ff0245fe15656
Andrii Shopiak <andriyshopyak@gmail.com>
2023-06-02 17:53:28 +0300

    feat: add rotate effect for burger when it clicked

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let menuBlock = document.querySelector(".header__menu");
 let burger = document.querySelector(".burger");
+let burgerCon = document.querySelector(".burger_con");
 
 burger.addEventListener("click", () => {
     menuBlock.classList.toggle("header__menu_active");
+    burgerCon.classList.toggle("burger_con_active");
 });
