a094bda51f9e8458fb17bb30eb1230da4cb415cb
Andrii Shopiak <andriyshopyak@gmail.com>
2023-06-04 23:06:28 +0300

    feat: add opacity for welcome when page load

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 let menuBlock = document.querySelector(".header__menu");
 let burger = document.querySelector(".burger");
 let burgerCon = document.querySelector(".burger_con");
+let welcomeBlock = document.querySelector(".welcome");
+
+window.addEventListener("load", () => {
+    welcomeBlock.classList.add("welcome__active");
+});
 
 burger.addEventListener("click", () => {
     menuBlock.classList.toggle("header__menu_active");
