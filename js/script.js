8ef18c105b48898d149545a1c2a91f7b65afbe98
Andrii Shopiak <andriyshopyak@gmail.com>
2023-06-10 11:23:07 +0300

    feat: start adding script for slider

--- js/script.js
+++ js/script.js
@@ -14,3 +14,65 @@ burger.addEventListener("click", () => {
     menuBlock.classList.toggle("header__menu_active");
     burgerCon.classList.toggle("burger_con_active");
 });
+
+// Slider functional
+
+let sliderBlocks = [`<div class="welcome_con" id="welcome_con">
+<div class="welcome_con__rigth right">
+    <div class="right__block">
+        <img src="img/gallery/house_one.jpg" alt="House">
+    </div>
+</div>
+<div class="welcome_con__left left">
+    <h2 class="left__title">
+        Будинок №1
+    </h2>
+    <p class="left__describe">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam molestias dolores at similique incidunt corrupti perspiciatis quae voluptatem doloremque architecto.
+    </p>
+    <button class="left__btn">
+        Більше
+    </button>
+</div>
+</div>`,`<div class="welcome_con" id="welcome_con">
+<div class="welcome_con__rigth right">
+    <div class="right__block">
+        <img src="img/gallery/house_two.jpg" alt="House">
+    </div>
+</div>
+<div class="welcome_con__left left">
+    <h2 class="left__title">
+        Будинок №2
+    </h2>
+    <p class="left__describe">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam molestias dolores at similique incidunt corrupti perspiciatis quae voluptatem doloremque architecto.
+    </p>
+    <button class="left__btn">
+        Більше
+    </button>
+</div>
+</div>`,`<div class="welcome_con" id="welcome_con">
+<div class="welcome_con__rigth right">
+    <div class="right__block">
+        <img src="img/gallery/house_one.jpg" alt="House">
+    </div>
+</div>
+<div class="welcome_con__left left">
+    <h2 class="left__title">
+        Будинок №3
+    </h2>
+    <p class="left__describe">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam molestias dolores at similique incidunt corrupti perspiciatis quae voluptatem doloremque architecto.
+    </p>
+    <button class="left__btn">
+        Більше
+    </button>
+</div>
+</div>`];
+let leftBtn = document.querySelector("#left__button");
+let rightBtn = document.querySelector("#right__button");
+let slider_case = document.querySelector(".gallery_slider");
+let count = 0;
+ leftBtn.addEventListener("click", () => {
+    
+ });
